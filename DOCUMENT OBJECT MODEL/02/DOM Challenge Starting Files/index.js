02879d952bba6ebb2447016edda24e93777c127c
Priyank Shah <83683507+p1209s@users.noreply.github.com>
2025-08-08 16:24:56 -0400

    update DOM

--- DOCUMENT OBJECT MODEL/02/DOM Challenge Starting Files/index.js
+++ DOCUMENT OBJECT MODEL/02/DOM Challenge Starting Files/index.js
@@ -3,3 +3,17 @@ document.query;
 // document.querySelector("ul").lastElementChild.innerHTML = "Priyank";
 // document.querySelector("ul .list a").style.color = "red";
 // document.querySelector("li").style.color = "red";
+// document.querySelector("button").style.backgroundColor = "yellow";
+// document.querySelector("h1").classList.add("huge");
+// document.querySelector("h1").classList.remove("huge");
+// document.querySelector("h1").classList.toggle("huge");
+// document.querySelector("a").innerHTML = "<em>Google me!</em>";
+
+// document.querySelector("a");
+// <a href=​"https:​/​/​www.google.com">​Google​</a>​
+// document.querySelector("a").attributes;
+// NamedNodeMap {0: href, href: href, length: 1}
+// document.querySelector("a").getAttribute("href");
+// 'https://www.google.com'
+// document.querySelector("a").setAttribute("href", "https://www.bing.com");
+// undefined
